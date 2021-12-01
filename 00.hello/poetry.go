0b6b1f49dac6b0fc4259a1a8ae4bdc1fd230efab
First_Song <877053710@qq.com>
2021-12-01 19:58:18 +0800

    update 00.hello

---
+++ 00.hello/poetry.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("  静夜思  ")
+	fmt.Println("   李白")
+	fmt.Println("窗前明月光")
+	fmt.Println("疑是地上霜")
+	fmt.Println("举头望明月")
+	fmt.Println("低头思故乡")
+}
