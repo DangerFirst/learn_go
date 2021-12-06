80926887ff52e29450c664ce3a84c473db346efa
First_Song <877053710@qq.com>
2021-12-06 22:56:01 +0800

    实现基本的平行判断

--- 02.fatrate and parallel/parallel.go
+++ 02.fatrate and parallel/parallel.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var x1, y1, x2, y2, x3, y3, x4, y4 int
+	//定义变量
+	var x1, y1, x2, y2, x3, y3, x4, y4, k1, k2 float64
+	//输入参数
 	fmt.Print("输入直线一其中一端点x轴方向的坐标：")
 	fmt.Scanln(&x1)
 	fmt.Print("输入直线一另一端点y轴方向的坐标：")
@@ -20,5 +22,13 @@ func main() {
 	fmt.Scanln(&x4)
 	fmt.Print("输入直线二另一端点y轴方向的坐标：")
 	fmt.Scanln(&y4)
-
+	//计算直线斜率
+	k1 = (y2 - y1) / (x2 - x1)
+	k2 = (y4 - y3) / (x4 - x3)
+	//判断两条直线是否平行
+	if k1 == k2 {
+		fmt.Println("两条直线平行")
+	} else {
+		fmt.Println("两条线不平行")
+	}
 }
