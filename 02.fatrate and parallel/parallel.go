5fbf9e832d875b5449b6b48d6a74cc59c5a4344c
First_song <877053710@qq.com>
2021-12-06 17:55:30 +0800

    修改文件命名并且增加判断平行项目

---
+++ 02.fatrate and parallel/parallel.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x1, y1, x2, y2, x3, y3, x4, y4 int
+	fmt.Print("输入直线一其中一端点x轴方向的坐标：")
+	fmt.Scanln(&x1)
+	fmt.Print("输入直线一另一端点y轴方向的坐标：")
+	fmt.Scanln(&y1)
+	fmt.Print("输入直线一其中一端点x轴方向的坐标：")
+	fmt.Scanln(&x2)
+	fmt.Print("输入直线一另一端点y轴方向的坐标：")
+	fmt.Scanln(&y2)
+	fmt.Print("输入直线二其中一端点x轴方向的坐标：")
+	fmt.Scanln(&x3)
+	fmt.Print("输入直线二另一端点y轴方向的坐标：")
+	fmt.Scanln(&y3)
+	fmt.Print("输入直线二其中一端点x轴方向的坐标：")
+	fmt.Scanln(&x4)
+	fmt.Print("输入直线二另一端点y轴方向的坐标：")
+	fmt.Scanln(&y4)
+
+}
