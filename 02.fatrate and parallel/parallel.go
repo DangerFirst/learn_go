d4b683c31d63a0e69b17a8e1c4a8b3c97d404b07
First_song <877053710@qq.com>
2021-12-07 16:46:28 +0800

    增加两条直线重合判断

--- 02.fatrate and parallel/parallel.go
+++ 02.fatrate and parallel/parallel.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	//定义变量
-	var x1, y1, x2, y2, x3, y3, x4, y4, k1, k2 float64
+	var x1, y1, x2, y2, x3, y3, x4, y4, k1, k2, b1, b2 float64
 	//输入参数
 	fmt.Print("输入直线一其中一端点x轴方向的坐标：")
 	fmt.Scanln(&x1)
@@ -25,18 +25,36 @@ func main() {
 	fmt.Printf("第一条直线的两点坐标为：(%.f,%.f),(%.f,%.f)\n", x1, y1, x2, y2)
 	fmt.Printf("第二条直线的两点坐标为：(%.f,%.f),(%.f,%.f)\n", x3, y3, x4, y4)
 	//判断直线是否平行
-	//斜率都存在时
-	if x1 != x2 && x3 != x4 {
+	//输入的直线坐标点相同时
+	if (x1 == x2 && x2 == y1 && y1 == y2) || (x3 == x4 && x4 == y3 && y3 == y4) {
+		fmt.Println("输入的参数非直线，无法判断")
+		//斜率都存在时
+	} else if x1 != x2 && x3 != x4 {
 		k1 = (y2 - y1) / (x2 - x1)
 		k2 = (y4 - y3) / (x4 - x3)
 		if k1 == k2 {
-			fmt.Println("两条直线平行")
+			//直线方程式y=kx+b
+			b1 = y1 - k1*x1
+			b2 = y3 - k2*x3
+			//判断两条线段是否在一条直线方程上
+			//一条直线方程上有重合的点
+			if b1 == b2 && ((x1 > x2 && ((x3 >= x2 && x3 <= x1) || (x4 >= x2 && x4 <= x1))) || (x1 < x2 && ((x3 >= x1 && x3 <= x2) || (x4 >= x1 && x4 <= x2)))) {
+				fmt.Println("两条直线不平行，且有重合")
+			} else {
+				fmt.Println("两条直线平行")
+			}
 		} else {
 			fmt.Println("两条直线不平行")
 		}
 		//斜率都不存在时
 	} else if x1 == x2 && x3 == x4 {
-		fmt.Println("两条直线平行")
+		//判断两条线段是否在一条直线方程上
+		//一条直线方程上有重合的点
+		if x1 == x3 && ((y1 > y2 && ((y3 >= y2 && y3 <= y1) || (y4 >= y2 && y4 <= y1))) || (y1 < y2 && ((y3 >= y1 && y3 <= y2) || (y4 >= y1 && y4 <= y2)))) {
+			fmt.Println("两条直线不平行,垂直于x轴且有重合")
+		} else {
+			fmt.Println("两条直线平行,且垂直于x轴")
+		}
 		//其中一条斜率不存在时
 	} else {
 		fmt.Println("两条直线不平行")
