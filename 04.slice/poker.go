1250a247c21ec581f633825b9c644bd2c2dc8f21
First_song <877053710@qq.com>
2021-12-14 18:19:39 +0800

    实现打乱牌组功能

--- 04.slice/poker.go
+++ 04.slice/poker.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
+	"time"
 )
 
 func main() {
 	//定义牌组数组
-	initPoker := []string{}
+	var initPoker, temPoker []string
 	//调用初始化牌组函数
-	initPoker=initPokerOrder()
+	initPoker = initPokerOrder()
 	//打印初始化的牌组数组
-	fmt.Println("初始牌组为：",initPoker)
+	fmt.Println("初始牌组为：", initPoker)
 	//洗牌
-
+	//洗切牌重复五次
+	for t := 0; t < 5; t++ {
+		//将牌分为两叠，然后均匀洗在一起
+		for site := 0; site < 27; site++ {
+			temPoker = append(temPoker, initPoker[site], initPoker[site+27])
+		}
+		//切5次牌
+		for cut := 0; cut < 5; cut++ {
+			rand.Seed(time.Now().Unix())
+			cutSite := rand.Intn(54)
+			temPoker = append(temPoker[cutSite:], temPoker[:cutSite]...)
+		}
+		initPoker = temPoker
+		//初始化临时数组
+		temPoker = nil
+	}
+	fmt.Println("打乱牌组为：", initPoker)
 }
 
 //初始化牌组
@@ -33,6 +51,6 @@ func initPokerOrder() (initPoker []string) {
 			}
 		}
 	}
-	initPoker = append(initPoker, "大王","小王")
+	initPoker = append(initPoker, "大王", "小王")
 	return initPoker
 }
