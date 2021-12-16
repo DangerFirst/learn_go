3c5460c8eae59f1ac3981967b9219a5c134311bd
First_song <877053710@qq.com>
2021-12-16 15:48:05 +0800

    实现转换字母大小写功能

--- 04.slice/transformation.go
+++ 04.slice/transformation.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	//定义要转换的数组
 	slice := []string{"Hello小明", "小case"}
+	fmt.Println("转换前数组为：", slice)
 	//调用转换函数
 	transformation(slice)
 	//输出转换后的数组
-	fmt.Println(slice)
+	fmt.Println("转换后数组为：", slice)
 }
 
+//转换大小写函数
 func transformation(beforTrans []string) (afferTrans string) {
-	for _, sliceContent := range beforTrans {
-		for _, cutString := range strings.Split(sliceContent, "") {
-			fmt.Println(cutString)
-		}
+	for contentSit, sliceContent := range beforTrans {
+		beforTrans[contentSit] = strings.ToUpper(sliceContent)
 	}
 	return
 }
