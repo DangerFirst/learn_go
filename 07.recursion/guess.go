4a3fff580771063d636b29c08d52ed801689cc80
First_song <877053710@qq.com>
2021-12-22 10:52:55 +0800

    猜数字游戏，修复猜低越界的问题

--- 07.recursion/guess.go
+++ 07.recursion/guess.go
@@ -1,24 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func guess(left, right uint) {
 	guessed := (left + right) / 2
 	fmt.Println("我猜的数字为：", guessed)
+	fmt.Println("left and right and guessed:", left, right, guessed)
 	fmt.Print("如果低了输入0，如果高了输入1，如果对了输入9：")
 	var getFromInput string
 	fmt.Scanln(&getFromInput)
 	switch getFromInput {
 	case "0":
-		fmt.Println("left and right:", left, guessed)
 		if left == right {
 			fmt.Println("你是不是改主意了")
 			return
 		}
 		guess(guessed+1, right)
 	case "1":
-		fmt.Println("left and right:", left, guessed)
-		if left == right {
+		if left == right || left == guessed {
 			fmt.Println("你是不是改主意了")
 			return
 		}
