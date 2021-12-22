56edb13d0fc363299f3b34314abacd7123f88c33
First_song <877053710@qq.com>
2021-12-22 14:49:25 +0800

    提取main步骤，精简主函数

---
+++ 06.fatrate_restructure/store/storematerial.go
@@ -0,0 +1,21 @@
+package store
+
+import (
+	"fmt"
+	"learn.go/06.fatrate_restructure/calc"
+	"learn.go/06.fatrate_restructure/input"
+)
+
+func StoreMaterial(num int)(names, sexs [999]string, bmis, fatRates [999]float64,ages [999]int, fatRateSum float64){
+	var talls, weights [999]float64
+	var sexWeights [999]int
+	//获取所有人的参数
+	for i := 0; i < num; i++ {
+		fmt.Printf("输入第%v个人的姓名：", i+1)
+		names[i], sexs[i], talls[i], weights[i], ages[i], sexWeights[i] = input.MaterialFromInput()
+		bmis[i]=calc.CalcuBmi(weights[i], talls[i])
+		fatRates[i] = calc.CalcuFatrate(ages[i], sexWeights[i],bmis[i])
+		fatRateSum += fatRates[i]
+	}
+return
+}
--- 06.fatrate_restructure/suggest/suggest.go
+++ 06.fatrate_restructure/suggest/suggest.go
@@ -2,17 +2,20 @@ package suggest
 
 import "fmt"
 
-func Suggest(name, sex string, age int, bmi, fatRate float64) {
-	fmt.Println("姓名：", name)
-	fmt.Printf("BMI：%.2f\n", bmi)
-	fmt.Printf("体脂率：%.2f\n", fatRate)
-	fmt.Print("评估及建议：")
-	if sex == "男" {
-		//男性体脂建议
-		suggestForMale(age, fatRate)
-	} else {
-		//女性体脂建议
-		suggestForFemale(age, fatRate)
+func Suggest(names, sexs [999]string, ages [999]int, num int, bmis, fatRates [999]float64) {
+	for j := 0; j < num; j++ {
+		fmt.Println("姓名：", names[j])
+		fmt.Printf("BMI：%.2f\n", bmis[j])
+		fmt.Printf("体脂率：%.2f\n", fatRates[j])
+		fmt.Print("评估及建议：")
+		if sexs[j] == "男" {
+			//男性体脂建议
+			suggestForMale(ages[j], fatRates[j])
+		} else {
+			//女性体脂建议
+			suggestForFemale(ages[j], fatRates[j])
+		}
+
 	}
 	return
 }
