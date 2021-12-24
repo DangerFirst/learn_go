021425de80de86e87902c95204c159382a3f1e02
First_song <877053710@qq.com>
2021-12-24 14:07:53 +0800

    使用cobra包输入输出参数

--- 08.read/main.go
+++ 08.read/main.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/spf13/cobra"
-	"os"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	var (
 		name   string
 		sex    string
-		age    string //int
-		tall   string //float64
-		weight string //float64
+		age    int
+		tall   float64
+		weight float64
 	)
-	arguments := os.Args
-	name = arguments[1]
-	sex = arguments[2]
-	age = arguments[3]
-	tall = arguments[4]
-	weight = arguments[5]
-	fmt.Println(arguments, len(arguments), cap(arguments))
-	fmt.Println("name：", name)
-	fmt.Println("sex：", sex)
-	fmt.Println("age：", age)
-	fmt.Println("tall：", tall)
-	fmt.Println("weight：", weight)
+	cmd:=&cobra.Command{
+		Use:                        "",
+		Short:                      "",
+		Long:                       "",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("name：", name)
+			fmt.Println("sex：", sex)
+			fmt.Println("age：", age)
+			fmt.Println("tall：", tall)
+			fmt.Println("weight：", weight)
+		},
+	}
+	cmd.Flags().StringVar(&name,"name1","","姓名")
+	cmd.Flags().StringVar(&sex,"sex","","性别")
+	cmd.Flags().IntVar(&age,"age",0,"年龄")
+	cmd.Flags().Float64Var(&tall,"tall",0,"身高")
+	cmd.Flags().Float64Var(&weight,"weight",0,"体重")
+	cmd.Execute()
 }
