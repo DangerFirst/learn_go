21751a5369b83e73553a74eab69523c71f9e78ca
First_Song <877053710@qq.com>
2021-12-27 00:08:34 +0800

    添加 案例3： • 王强录入体脂是38 • 李静录入体脂是28 • 张伟注册成功，未录入体脂 • 李静排名第一，体脂28；王强排名第二，体脂38；张伟排名第三，没有体脂记录。

--- 10.tdd/main_test.go
+++ 10.tdd/main_test.go
@@ -72,3 +72,33 @@ func TestCase2(t *testing.T) {
 		}
 	}
 }
+
+func TestCase3(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("张伟")
+	inputRecord("李静", 0.28)
+	{
+		randOfWQ, fatRateOfWQ := getRank("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期王强第二，但是得到的是%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.38 {
+			t.Fatalf("预期王强的体脂率为0.38，但是得到的是%f", fatRateOfWQ)
+		}
+	}
+	{
+		randOfZW, _ := getRank("张伟")
+		if randOfZW != 3 {
+			t.Fatalf("预期张伟第三，但是得到的是%d", randOfZW)
+		}
+	}
+	{
+		randOfLJ, fatRateOfLJ := getRank("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期李静第一，但是得到的是%d", randOfLJ)
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂率为0.28，但是得到的是%f", fatRateOfLJ)
+		}
+	}
+}
