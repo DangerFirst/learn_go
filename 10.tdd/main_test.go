432b309dcc35e493f59a414a1ea9b0a14ca3579d
First_Song <877053710@qq.com>
2021-12-26 23:35:28 +0800

    添加 案例2： • 王强录入体脂是38 • 张伟录入体脂是38 • 李静录入体脂是28 • 李静排名第一，体脂28；王强、张伟排名第二，体脂38。

--- 10.tdd/main_test.go
+++ 10.tdd/main_test.go
@@ -18,6 +18,39 @@ func TestCase1Part1(t *testing.T) {
 
 func TestCase1(t *testing.T) {
 	inputRecord("王强", 0.38)
+	inputRecord("张伟", 0.38)
+	inputRecord("李静", 0.28)
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期王强第二，但是得到的是%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.38 {
+			t.Fatalf("预期王强的体脂率为0.38，但是得到的是%f", fatRateOfWQ)
+		}
+	}
+	{
+		randOfZW, fatRateOfZW := getRand("张伟")
+		if randOfZW != 2 {
+			t.Fatalf("预期张伟第二，但是得到的是%d", randOfZW)
+		}
+		if fatRateOfZW != 0.38 {
+			t.Fatalf("预期张伟的体脂率为0.38，但是得到的是%f", fatRateOfZW)
+		}
+	}
+	{
+		randOfLJ, fatRateOfLJ := getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期李静第一，但是得到的是%d", randOfLJ)
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂率为0.28，但是得到的是%f", fatRateOfLJ)
+		}
+	}
+}
+
+func TestCase2(t *testing.T) {
+	inputRecord("王强", 0.38)
 	inputRecord("王强", 0.32)
 	inputRecord("李静", 0.28)
 	{
@@ -38,8 +71,4 @@ func TestCase1(t *testing.T) {
 			t.Fatalf("预期李静的体脂率为0.28，但是得到的是%f", fatRateOfLJ)
 		}
 	}
-}
-
-func TestCase2(t *testing.T) {
-
 }
