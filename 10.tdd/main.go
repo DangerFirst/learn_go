534929036a995b7fc90e8a724f761e3bbc7e8129
First_Song <877053710@qq.com>
2021-12-27 00:05:37 +0800

    完成案例2

--- 10.tdd/main.go
+++ 10.tdd/main.go
@@ -10,20 +10,22 @@ func inputRecord(name string, fatRate float64) {
 	personFatRate[name] = fatRate
 }
 
-func getRand(name string) (rand int, fateRate float64) {
-	fateRate2PersonMap := map[float64]string{}
-	randArr := make([]float64, 0, len(personFatRate))
+func getRank(name string) (rank int, fateRate float64) {
+	fateRate2PersonMap := map[float64][]string{}
+	rankArr := make([]float64, 0, len(personFatRate))
 	for nameItem, frItem := range personFatRate {
-		fateRate2PersonMap[frItem] = nameItem
-		randArr = append(randArr, frItem)
+		fateRate2PersonMap[frItem] = append(fateRate2PersonMap[frItem], nameItem)
+		rankArr = append(rankArr, frItem)
 	}
-	sort.Float64s(randArr)
-	for i, frItem := range randArr {
-		_name := fateRate2PersonMap[frItem]
-		if _name == name {
-			rand = i + 1
-			fateRate = frItem
-			return
+	sort.Float64s(rankArr)
+	for i, frItem := range rankArr {
+		_names := fateRate2PersonMap[frItem]
+		for _, _name := range _names {
+			if _name == name {
+				rank = i + 1
+				fateRate = frItem
+				return
+			}
 		}
 	}
 	return
