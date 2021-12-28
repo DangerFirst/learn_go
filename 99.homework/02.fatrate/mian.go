4263367edf23771857c3724258b2a8fd6c8be0d9
First_Song <877053710@qq.com>
2021-12-28 22:03:54 +0800

    添加可以再次输入功能

--- 99.homework/02.fatrate/mian.go
+++ 99.homework/02.fatrate/mian.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"learn.go/99.homework/02.fatrate/calc"
+	"learn.go/99.homework/02.fatrate/continul"
 	"learn.go/99.homework/02.fatrate/input"
 	"learn.go/99.homework/02.fatrate/suggest"
 )
 
 func main() {
-	//获取参数
-	person := input.GetMaterialFromInput()
-	//计算BMI
-	calc.Calc{}.BMI(person)
-	//计算体脂率
-	calc.Calc{}.FatRate(person)
-	//获取体脂建议
-	suggest.Suggest(person)
+	for { //获取参数
+		person := input.GetMaterialFromInput()
+		//计算BMI
+		calc.Calc{}.BMI(person)
+		//计算体脂率
+		calc.Calc{}.FatRate(person)
+		//获取体脂建议
+		suggest.Suggest(person)
+		//是否继续
+		if continul.WeitherContinul() {
+			return
+		}
+	}
 }
