cedbd4c2ea0da8e707f5defb967de45f73e56e1a
First_Song <877053710@qq.com>
2022-01-11 21:33:28 +0800

    简单实现电梯功能

--- 99.homework/03.elevator/elevartor_run.go
+++ 99.homework/03.elevator/elevartor_run.go
@@ -1,9 +1,14 @@
 package main
 
-func main() {
-	//elevator:=&Elevator{
-	//	floor:        5,
-	//	currentFloor: 1,
-	//}
+import "fmt"
 
+func main() {
+	elevator := &Elevator{
+		floor:        5,
+		currentFloor: 3,
+	}
+	fmt.Println("电梯当前楼层为：", elevator.currentFloor)
+	elevator.InputToFloor(4, 1, 2, 5)
+	elevator.ToFloor()
+	fmt.Printf("电梯停在：%d层\n", elevator.currentFloor)
 }
--- 99.homework/03.elevator/elevator.go
+++ 99.homework/03.elevator/elevator.go
@@ -1,17 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
 type Elevator struct {
 	floor        int
 	currentFloor int
-	task         []int
+	ElevatorTask
+}
+
+type ElevatorTask struct {
+	upTask   []int
+	downTask []int
 }
 
 func (e *Elevator) InputToFloor(floor ...int) {
-	e.task = append(e.task, floor...)
+	task := &e.ElevatorTask
+	for _, item := range floor {
+		if item > e.currentFloor {
+			task.upTask = append(task.upTask, item)
+		} else {
+			task.downTask = append(task.downTask, item)
+		}
+	}
+	sort.Slice(task.upTask, func(i, j int) bool {
+		return task.upTask[i] < task.upTask[j]
+	})
+	sort.Slice(task.downTask, func(i, j int) bool {
+		return task.downTask[i] > task.downTask[j]
+	})
 }
 
 func (e *Elevator) ToFloor() {
-	for i, item := range e.task {
-
+	task := []int{}
+	if len(e.ElevatorTask.upTask) >= len(e.ElevatorTask.downTask) {
+		task = append(e.ElevatorTask.upTask, e.ElevatorTask.downTask...)
+	} else {
+		task = append(e.ElevatorTask.downTask, e.ElevatorTask.upTask...)
+	}
+	for _, item := range task {
+		time.Sleep(time.Second)
+		fmt.Printf("电梯到达第%d层\n", item)
+		e.currentFloor = item
 	}
 }
