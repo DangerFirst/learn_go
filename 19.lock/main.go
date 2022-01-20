8446c43fb58a8e50986b02fa0c351776b2a550cd
First_song <877053710@qq.com>
2022-01-20 11:33:08 +0800

    使用sync.RWMutex读写锁，RLock时，允许一起读，但不允许写

--- 19.lock/main.go
+++ 19.lock/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	for i := 0; i < 5; i++ {
 		//countDict()
-		countDictGoroutineSafe()
+		//countDictGoroutineSafe()
+		countDictGoroutineSafeRW()
 	}
 }
 
@@ -47,4 +49,38 @@ func countDictGoroutineSafe() {
 	wg.Wait()
 	fmt.Println("预计有", 5000*100, "个字")
 	fmt.Println("总共有", TotalCount, "个字")
+}
+
+func countDictGoroutineSafeRW() {
+	fmt.Println("开始数")
+	TotalCount := 0
+	TotalCountLock := sync.RWMutex{}
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for p := 0; p < 5; p++ {
+		go func(p int) {
+			defer wg.Done()
+			fmt.Println(p, "读锁开始时间：", time.Now())
+			TotalCountLock.RLock()
+			fmt.Println(p, "读锁拿到锁时间：", time.Now())
+			time.Sleep(time.Second)
+			defer TotalCountLock.RUnlock()
+			//fmt.Print("正在统计第", p, "页，")
+			TotalCount += 100
+		}(p)
+	}
+
+	for p := 0; p < 5; p++ {
+		go func(p int) {
+			defer wg.Done()
+			fmt.Println(p, "写锁开始时间：", time.Now())
+			TotalCountLock.Lock()
+			fmt.Println(p, "写锁拿到锁时间：", time.Now())
+			defer TotalCountLock.Unlock()
+			//fmt.Print("正在统计第", p, "页，")
+			TotalCount += 100
+		}(p)
+	}
+	wg.Wait()
+
 }
