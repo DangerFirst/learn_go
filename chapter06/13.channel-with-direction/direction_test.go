67ad5754407e28c36766048c1bb05b3f3256db19
First_Song <877053710@qq.com>
2022-02-15 13:29:21 +0800

    单向channel

---
+++ chapter06/13.channel-with-direction/direction_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestWithDirection(t *testing.T) {
+	c := make(chan int, 100)
+	inOnly(c)
+	outOnly(c)
+}
+
+func inOnly(c chan<- int) {
+	c <- 1
+}
+
+func outOnly(c <-chan int) {
+	<-c
+}
