a47f0d0cda16f41f482c8a85cd101c9c64f034ca
DangerFirst <877053710@qq.com>
2022-03-28 22:26:11 +0800

    用http实现体脂计算器

--- pkg/apis/types.go
+++ pkg/apis/types.go
@@ -1,13 +1,25 @@
 package apis
 
-type PersonalInformation struct {
-	name          string  `json:"name"` //注意，私有的成员变量在序列化、反序列化时会被忽略
-	Name          string  `json:"name"`
-	Sex           string  `json:"sex"`
-	Age           int     `json:"age"`
-	Tall          float64 `json:"tall"`
-	Weight        float64 `json:"weight"`
-	state         interface{}
-	sizeCache     interface{}
-	unknownFields interface{}
+//type PersonalInformation struct {
+//	name          string  `json:"name"` //注意，私有的成员变量在序列化、反序列化时会被忽略
+//	Name          string  `json:"name"`
+//	Sex           string  `json:"sex"`
+//	Age           int     `json:"age"`
+//	Tall          float64 `json:"tall"`
+//	Weight        float64 `json:"weight"`
+//	state         interface{}
+//	sizeCache     interface{}
+//	unknownFields interface{}
+//}
+
+type PersonalInformationFatRate struct {
+	Name    string
+	FatRate float64
+}
+
+type PersonalRank struct {
+	Name    string
+	Sex     string
+	RankNum int
+	FatRate float64
 }
