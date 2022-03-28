daa5bf5736663df25785757005af4651d4a53fa8
DangerFirst <877053710@qq.com>
2022-03-28 17:59:54 +0800

    http基本操作

---
+++ chapter11/01.http_base/post/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("fooooo")
+	//bytes.NewReader([]byte(""))
+	resp, err := http.Post("https://www.baidu.com", "*/*", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(data))
+}
