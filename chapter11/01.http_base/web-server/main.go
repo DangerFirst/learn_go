0d720d7438d1a6a975eba6bdbd74e95a3a4e15ae
DangerFirst <877053710@qq.com>
2022-03-28 18:09:48 +0800

    从url中读数据 http://localhost:8088/?name=xianghei&&sex=%E7%94%B7

--- chapter11/01.http_base/web-server/main.go
+++ chapter11/01.http_base/web-server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"time"
 )
 
 func main() {
 	http.ListenAndServe(":8088", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		time.Sleep(2 * time.Second)
-		writer.Write([]byte("hello,你好"))
+		qp := request.URL.Query()
+		data, _ := json.Marshal(qp) //todo handle error
+		writer.Write([]byte("hello,你好:" + string(data)))
 	}))
 }
